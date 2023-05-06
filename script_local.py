a8a661345d8f8c3ea6adbf3f312c4f44e35a193f
Apiros3 <akikan.contact@gmail.com>
2023-05-06 18:39:26 +0100

    initial

---
+++ script_local.py
@@ -0,0 +1,63 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import random
+import datetime
+# import discord
+
+WEBHOOK_URL = "https://discord.com/api/webhooks/..."
+
+url = "https://kenkoooo.com/atcoder/resources/problem-models.json"
+
+response = requests.get(url)
+# print(response.content)
+soup = BeautifulSoup(response.content, 'lxml')
+dataset = json.loads(soup.get_text())
+# diff = ["\u26AA","\uD83D\uDFE4","\uD83D\uDFE2",""]
+
+def main():
+    print("running...")
+
+    message = []
+    for i in range(8):
+        tmp = get_query(400*i, 400*(i+1)-1)
+        message.append(tmp)
+    post_message(message)
+
+def get_query(low, hi):
+    # print(str(title))
+    lst = []
+    for info in dataset:
+        try:
+            tmp = dataset[info]["difficulty"]
+            if (low <= int(dataset[info]["difficulty"]) and int(dataset[info]["difficulty"]) <= hi):
+                lst.append(str(info))
+        except KeyError:
+            continue            
+
+    return (lst[random.randint(0,len(lst))])
+
+def post_message(message):
+    # sends message to discord server
+    payload = {
+        "username" : "Daily Problemset",
+        "embeds" : [
+            {
+                "title": f"Problemset for {datetime.datetime.today().strftime('%Y-%m-%d')}",
+                "description": "",
+                "fields" : []
+            }
+        ]
+    }
+    for i in range(8):
+        payload["embeds"][0]["fields"].append({
+            "name": f"Q{i+1}: ",
+            "value": f"[{message[i]}](https://atcoder.jp/contests/{message[i][0:len(message[i])-2]}/tasks/{message[i]})",
+            # "inline": "false"
+        })
+
+    with requests.post(WEBHOOK_URL, json=payload) as response :
+        print(response.status_code)
+    
+if __name__ == "__main__":
+    main()
